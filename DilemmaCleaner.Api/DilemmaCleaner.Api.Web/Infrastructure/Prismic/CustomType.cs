5ea87a6f382a90510b0f880c734ae726429d3d24
Michał Kutrzuba <michal.kutrzuba@gmail.com>
2022-08-21 01:32:15 +0200

    :sparkles: added class with all prismic custom types keys
    It will be easier to reuse them later in code.

---
+++ DilemmaCleaner.Api/DilemmaCleaner.Api.Web/Infrastructure/Prismic/CustomType.cs
@@ -0,0 +1,9 @@
+namespace DilemmaCleaner.Api.Web.Infrastructure.Prismic;
+
+public static class CustomType
+{
+    public static string Dilemma => "dilemma";
+    public static string DilemmaStep => "dilemma_step";
+    public static string Settings => "settings";
+    public static string Translations => "translations";
+}
