cde12fc6242a8f3863659341199e8c7858ebd2e9
Michał Kutrzuba <michal.kutrzuba@gmail.com>
2022-08-24 07:32:10 +0200

    :sparkles: add configuration fetch on root file

---
+++ DilemmaCleaner.FrontEnd/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useConfigurationStore } from '~/store/configuration'
+
+const configurationStore = useConfigurationStore()
+
+onMounted(async () => {
+  if (!configurationStore.configuration) {
+    await configurationStore.fetch()
+  }
+})
+</script>
